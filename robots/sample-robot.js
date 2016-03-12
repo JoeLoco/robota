818154e158fcf17a04b33d93bca894da2d5d7dcc
joeloco <joao.h.a.santos@gmail.com>
2016-03-11 21:08:16 -0300

    add creator

---
+++ robots/sample-robot.js
@@ -0,0 +1,22 @@
+var Robot = require('../lib/robot.js');
+
+var SampleRobot = function (id) {
+
+	Robot.call(this,id);
+	this.name = "Sample Robot";
+	
+};
+SampleRobot.prototype = Object.create(Robot.prototype);
+
+SampleRobot.prototype.onUpdate = function() {
+
+	// Overrride the update event
+	
+	this.moveRandom();
+	
+	console.log(this);
+	
+};
+
+
+module.exports = SampleRobot;
